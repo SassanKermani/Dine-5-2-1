164a230f3589c4de4ff35defeaaed68e27213979
anhonestmouse <anhonestmouse@gmail.com>
2018-01-11 16:15:02 -0700

    some fixes.  regular commit

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.get('/',(req,res)=>{
 //req.session.passport.user.id FTW!
 app.get('/newSession', (req,res)=>{
 	let options = {
-		url: 'https://api.yelp.com/v3/businesses/search?location=12955+Lafayette+St,Thornton,Co,80241&radius=8000&price=1,2,3&sort_by=rating&term=food&open_now=true&limit=30',
+		url: 'https://api.yelp.com/v3/businesses/search?location=12955+Lafayette+St,80241&radius=8000&price=1,2,3&sort_by=rating&term=food&open_now=true&limit=30',
 		auth:{
 			bearer: bearerToken
 		}
@@ -68,7 +68,7 @@ app.get('/newSession', (req,res)=>{
 				image: business.image_url,
 				website: business.url,
 				address: business.location.display_address,
-				phone: business.phone
+				phone: business.display_phone
 				//coupleId?
 			};
 			db.Restaurant.create(newRestaurant,(err, newRest)=>{
